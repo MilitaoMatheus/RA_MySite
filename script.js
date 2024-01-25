346ecd3a1d4f3489b9d6249f85f66ea31cdab70b
Matheus Militão <matheus.silva2533@etec.sp.gov.br>
2024-01-25 18:33:45 -0300

    Slide de localizaão

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function calcularIMC() {
     document.getElementById('resultado').innerText = imc.toFixed(2);
   }
   
+  /* Programação do Primeiro Slide */
+  const sliderContainer = document.querySelector('.slider-container');
+  const slides = document.querySelector('.slides');
+  const slideWidth = document.querySelector('.slide').offsetWidth;
+  const totalSlides = document.querySelectorAll('.slide').length;
+  let currentIndex = 0;
+  
+  document.querySelector('.arrow.left').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateSlidePosition();
+  });
+  
+  document.querySelector('.arrow.right').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateSlidePosition();
+  });
+  
+  function updateSlidePosition() {
+    slides.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
+  }
+  
+
